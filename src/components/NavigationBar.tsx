47ea4ed4447ea869814368a388c41f3b2dffbd4f
linuxMate <grigorikovalenko@gmail.com>
2024-02-08 18:33:02 +0300

    update spa

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {FC} from "react";
+import './NavigationBar.css'
+
+interface NavigationBarProps {
+}
+
+const NavigationBar: FC<NavigationBarProps> = ({}) => {
+
+    return (
+        <Navbar expand="sm" className="bg-white" data-bs-theme="dark">
+            <div className='container-xl px-2 px-sm-3'>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto justify-content-center">
+                        <Nav.Item>
+                            <Link to="/banknotes" className="custom-link ps-0">Купюры</Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    );
+}
+export default NavigationBar;
