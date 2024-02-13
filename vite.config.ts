d5f29bb9172af726567adcb31ea7c47ae24ae329
linuxMate <grigorikovalenko@gmail.com>
2024-02-14 00:43:26 +0300

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RIP_Frontend/",
+  base: "/RIP_frontend/",
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8080',
